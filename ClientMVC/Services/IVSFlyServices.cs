ba59cc7b5c4c28003f88b9b9fda5a0e86622254b
etdnp <emilierey.teodorodonasciment@students.hevs.ch>
2022-05-24 18:44:18 +0200

    MVC

---
+++ ClientMVC/Services/IVSFlyServices.cs
@@ -0,0 +1,13 @@
+﻿using ClientMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientMVC.Services
+{
+    public interface IVSFlyServices
+    {
+        public Task<IEnumerable<FlightModel>> GetFlights();
+    }
+}
