c7081c60ea019929c85a205f999a5bce2d92f065
etdnp <emilierey.teodorodonasciment@students.hevs.ch>
2022-05-29 15:19:36 +0200

    Client2WebApp

---
+++ Client2WebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ClientMVC.Models;
+using ClientMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IVSFlyServices _vsFly;
+
+        public HomeController(ILogger<HomeController> logger, IVSFlyServices vsFly)
+        {
+            _logger = logger;
+            _vsFly = vsFly;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            var listFlights = await _vsFly.GetFlights();
+            return View(listFlights);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
